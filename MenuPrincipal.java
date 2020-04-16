60475987e491b987b430d5ed3b03283bf1185df0
EdilsonFilhoRZ <63757183+EdilsonFilhoRZ@users.noreply.github.com>
2020-04-15 22:48:05 -0300

    Create MenuPrincipal.java

---
+++ MenuPrincipal.java
@@ -0,0 +1,20 @@
+package TelaInicial;
+
+import java.util.Scanner;
+
+
+
+public class MenuPrincipal {
+
+public static void main(String[] args) {
+try {
+	TiposDeMenu.MenuPrincipal();
+} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+	
+
+	}
+}
